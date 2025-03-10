c50805f8f4c8c38bf9b9f0fe534a85710e0bc53d
Rikki <rikki@member.fsf.org>
2025-03-10 15:52:20 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python311
+        ];
+        shellHook = ''
+          unset LD
+        '';
+      };
+  };
+}
